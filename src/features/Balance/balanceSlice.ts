8202a711af71564c34a182ea5ecb0b31b79504aa
Monroe Gollaher <mgollaher92@gmail.com>
2022-10-06 22:16:53 -0600

    transfer / withdraw

--- src/features/Balance/balanceSlice.ts
+++ src/features/Balance/balanceSlice.ts
@@ -13,23 +13,17 @@ export const balanceSlice = createSlice({
   name: 'balance',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.balance += 1;
-    },
-    decrement: (state) => {
-      state.balance -= 1;
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
+    deposit: (state, action: PayloadAction<number>) => {
       state.balance += action.payload;
     },
-    decrementByAmount: (state, action: PayloadAction<number>) => {
-      state.balance += action.payload;
+    withdraw: (state, action: PayloadAction<number>) => {
+      state.balance -= action.payload;
     },
   },
  
 });
 
-export const { increment, decrement, incrementByAmount, decrementByAmount } = balanceSlice.actions;
+export const { deposit, withdraw } = balanceSlice.actions;
 
 export const selectBalance = (state: RootState) => state.balance.balance;
 
