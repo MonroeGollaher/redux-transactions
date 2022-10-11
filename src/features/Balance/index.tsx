40a6425e2967361b58e1ef71a61225e6fe822b3f
Monroe Gollaher <mgollaher92@gmail.com>
2022-10-10 19:26:01 -0600

    more progress

--- src/features/Balance/index.tsx
+++ src/features/Balance/index.tsx
@@ -11,23 +11,28 @@ export function Balance() {
   const [transactionAmount, setTransActionAmount] = useState('0')
   const transactionValue = Number(transactionAmount) || 0
   return (
-    <div className={styles.component}>
-      <div className={styles.balance}>
-        <h2>Balance</h2>
-        <p>${balance}</p>
+    <>
+      <div className={styles.component}>
+        <div className={styles.buttonWrapper}>
+          <button onClick={() => dispatch(deposit(transactionValue))}>
+            Deposit
+          </button>
+          <button onClick={() => dispatch(withdraw(transactionValue))}>
+            Withdraw
+          </button>
+        </div>
+        <div className={styles.balance}>
+          <h2>Balance</h2>
+          <p>${balance}</p>
+        </div>
       </div>
-      <div className={styles.buttons}>
-        <button onClick={() => dispatch(deposit(transactionValue))}>
-          Deposit
-        </button>
-        <button onClick={() => dispatch(withdraw(transactionValue))}>
-          Withdraw
-        </button>
+      <div className={styles.input}>
+        <p>Enter withdrawal / transfer amount</p>
         <input 
           value={transactionAmount}
           onChange={(e) => setTransActionAmount(e.target.value)}
-        />
+          />
       </div>
-    </div>
+    </>
   )
 } 
